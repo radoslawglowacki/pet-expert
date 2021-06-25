b1953fe770e38b16f36101d20a54bc4de9c06ad4
radoslawglowacki <radglo1995@gmail.com>
2021-06-25 17:08:19 +0200

    Init commit

---
+++ src/main/java/com/radekdawid/petexpert/PetExpertApplication.java
@@ -0,0 +1,13 @@
+package com.radekdawid.petexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radekdawid/petexpert/PetexpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radekdawid.petexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetexpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
